d13947ff28b70437630895e9ccda0e8416431bd5
lin <1845948316@qq.com>
2021-03-28 18:20:12 +0800

    重新提交

--- src/router/index.js
+++ src/router/index.js
@@ -8,8 +8,6 @@ import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
 import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
-import List from '../components/goods/List.vue'
-import Add from '../components/goods/Add.vue'
 
 Vue.use(VueRouter)
 
@@ -50,14 +48,6 @@ const routes = [
       {
         path: '/params',
         component: Params
-      },
-      {
-        path: '/goods',
-        component: List
-      },
-      {
-        path: '/goods/add',
-        component: Add
       }
     ]
   }
